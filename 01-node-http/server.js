608ce8d52af5ffac306104a0c6cf90cc66ed1827
Artur Chagas <artur_life_2015@hotmail.com>
2025-10-04 18:23:17 -0300

    Servidor com node js

--- 01-node-http/server.js
+++ 01-node-http/server.js
@@ -1,9 +1,22 @@
 const http = require('node:http');
 
 const server = http.createServer((request, response) => {
-    console.log(request)
-    response.writeHead(200)
-    response.write('SErvidor HTTP em Node.js funcionando!')
+   const path = request.url
+
+   switch(path){
+    case '/':
+        response.writeHead(200)
+        response.write('Você está na página home !')
+        break
+    case '/artigos':
+        response.writeHead(200)
+        response.write('Você está na página de artigos !')
+        break
+    default:
+        response.writeHead(404)
+        response.write('Página não encontrada !')
+        break
+   }
     response.end()
 })
 
