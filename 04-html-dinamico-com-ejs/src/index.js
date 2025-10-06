2d4f474a424a9945d92a4cc3dee075afa15d20db
Artur Chagas <artur_life_2015@hotmail.com>
2025-10-06 16:55:14 -0300

    Cadastro de usuarios com express e ejs

--- 04-html-dinamico-com-ejs/src/index.js
+++ 04-html-dinamico-com-ejs/src/index.js
@@ -3,8 +3,14 @@ const path = require('path');
 
 const app = express();
 
+const storedUsers = [] 
+
+//configuracao do ejs
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
+
+//configuracao do body
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req,res)=>{
     const title = "Homepage"
@@ -14,6 +20,23 @@ app.get('/', (req,res)=>{
     res.render('index',{title, message})
 })
 
+app.get('/formulario',(req,res)=>{
+    res.render('form')
+})
+
+app.post('/register',(req,res)=>{
+    const username = req.body.username
+    const password = req.body.username
+    storedUsers.push({ username,password })
+
+    res.redirect('/usuarios')
+
+})
+
+
+app.get('/usuarios', (req, res)=>{
+    res.render('users',{ users:storedUsers })
+})
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Servidor inciado! Acesse http://localhost:${PORT}`));
